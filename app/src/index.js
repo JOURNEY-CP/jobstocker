5d1d5ae43e680980427b9c915540369a142afea1
CHILUKURI SRI HARSHA <sharshach@gmail.com>
2021-03-30 13:56:05 +0530

    service workers final

--- app/src/index.js
+++ app/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-firebase'
 import firebase from 'firebase';
 import firebaseConfig from './firebaseConfig';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
@@ -16,7 +16,12 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-serviceWorker.register();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
